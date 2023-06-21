db2df2323793ccfe60e045f0852d80b37989aa7d
ndubisi jnr <ndubisijnr@gmail.com>
2023-06-21 14:39:18 -0700

    updte

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include main.h
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+viod print_alphabet(viod)
+{
+
+	char letter
+	
+	for(letter = 'a'; letter<='z'; letter++)
+	{
+	
+		-putchar(letter);
+	}
+
+	_putchar('\n');
+}
