97be23c06545e0120a4dcf6aa99b6a4fc74b75d8
ndubisijnr <ndubisijnr@gmail.com>
2023-06-23 21:42:11 +0100

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,14 @@
  */
 void print_number(int n)
 {
-	_putchar(n);
-return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
