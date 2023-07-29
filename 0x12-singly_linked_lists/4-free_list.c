c6eb32db560f5821594c59776c72b8ae10d27b80
ndubisijnr <ndubisijnr@gmail.com>
2023-07-29 01:14:28 +0100

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,18 @@
 */
 void free_list(list_t *head)
 {
-list_t *temp;
+	list_t *current;
 
-while ((temp = head) != NULL)
-{
-temp = head->next;
-free(temp->str)
-free(temp);
-}
+	/**
+	 * while current head is not NULL
+	 * set head as the next node and
+	 * free the current node str then
+	 * free current node.
+	*/
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
