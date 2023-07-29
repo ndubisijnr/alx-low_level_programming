0d6b191dedef5ae32722e674a567977cd936988a
ndubisijnr <ndubisijnr@gmail.com>
2023-07-29 01:10:13 +0100

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,14 @@
  *
  * Return: nothing
 */
-void free_list(list_t* head) {
-    list_t* temp;
+void free_list(list_t *head)
+{
+list_t *temp;
 
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
 }
